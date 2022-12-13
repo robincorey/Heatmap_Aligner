ed6c05b5f8b7f32bec352badc43193566d0db723
robincorey <robin.corey@gmail.com>
2022-12-13 16:12:12 +0000

    starting aligner script

--- Heatmap_Aligner.py
+++ Heatmap_Aligner.py
@@ -1,1 +1,35 @@
-#
+#!/usr/bin/env python3
+
+#import sys
+#import os
+#import biopython
+
+'''
+Script to take multiple PyLipID outputs and create a sequence alignment coloured by lipid statistics
+Can be used with a single PyLipID output just to make a sequence-based heatmap
+'''
+
+############################
+### Defining input files ###
+############################
+
+# Where files are
+dir = '/sansom/s156a/bioc1535/MraY/EC_other_protein/Data'
+
+# List of input csv files
+csvfile_list = ["%s/Interaction_UDP1_DEDA/Dataset_UDP1/Dataset.csv" % dir, 
+		"%s/Interaction_UDP1_UPTA/Dataset_UDP1/Dataset.csv" % dir ]
+
+
+#########################
+### Setting variables ###
+#########################
+
+
+
+#######################################
+### Don't touch anything below here ###
+#######################################
+
+for csvfile in csvfile_list:
+	print(csvfile)
