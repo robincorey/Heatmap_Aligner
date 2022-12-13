b0719fcb284750ae7765ac1898412769756703d4
robincorey <robin.corey@gmail.com>
2022-12-13 16:28:55 +0000

    functions for aligner script

--- Heatmap_Aligner.py
+++ Heatmap_Aligner.py
@@ -17,13 +17,16 @@ Can be used with a single PyLipID output just to make a sequence-based heatmap
 dir = '/sansom/s156a/bioc1535/MraY/EC_other_protein/Data'
 
 # List of input csv files
-csvfile_list = ["%s/Interaction_UDP1_DEDA/Dataset_UDP1/Dataset.csv" % dir, 
-		"%s/Interaction_UDP1_UPTA/Dataset_UDP1/Dataset.csv" % dir ]
+csvfile_list = [ "%s/Interaction_UDP1_DEDA/Dataset_UDP1/Dataset.csv" % dir,
+		 "%s/Interaction_UDP1_UPTA/Dataset_UDP1/Dataset.csv" % dir ]
 
+# List of input coordinate files - can be CG or AT
+coord_files = [	"%s/Interaction_UDP1_UPTA/Coordinate_UDP1/Coordinate_UDP1_Occupancy.pdb" % dir,
+		"%s/Interaction_UDP1_UPTA/Coordinate_UDP1/Coordinate_UDP1_Occupancy.pdb" % dir ]
 
-#########################
-### Setting variables ###
-#########################
+##########################
+### Set variables here ###
+##########################
 
 
 
@@ -31,5 +34,30 @@ csvfile_list = ["%s/Interaction_UDP1_DEDA/Dataset_UDP1/Dataset.csv" % dir,
 ### Don't touch anything below here ###
 #######################################
 
+def get_number_of_systems():
+	csv_number = len(csvfile_list)
+	coord_number = len(coord_files)
+	if csv_number == coord_number:
+		
+		print('%s input files being read' % csv_number)
+	else:
+		print('mismatch in input files, %s csv files and %s coordinate files' % (csv_number, coord_number))
+
+def sequence_alignment():
+	pass
+
+def get_lipid_heatmap():
+	pass
+
+def plot_heatmap_on_alignment():
+	pass
+
 for csvfile in csvfile_list:
-	print(csvfile)
+	#print(csvfile)
+	pass
+
+######################
+### Run everything ###
+######################
+
+get_number_of_systems()
