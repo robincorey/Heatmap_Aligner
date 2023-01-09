742cc046228cf4cdcf2cf1c760cfe87ebb20ae15
robincorey <robin.corey@gmail.com>
2023-01-09 11:17:12 +0000

    Combining plotting sections into main script

--- Heatmap_Aligner.py
+++ Heatmap_Aligner.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import string
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import csv
 import re
@@ -17,24 +21,15 @@ Written by Robin Corey
 ### Defining input files - USER INPUT NEEDED ###
 
 # Where files are - currently this test dir
-dir = '/sansom/s137/bioc1535/Desktop/git-repos/Heatmap_Aligner/'
+#dir = '/sansom/s137/bioc1535/Desktop/git-repos/Heatmap_Aligner/'
+dir='/Volumes/GoogleDrive/My Drive/Oxford_projects/HeatmapAlignment/Heatmap_Aligner'
 
 # List of input csv files
-csvfile_list = [ "%s/DEDA/Dataset.csv" % dir,
-		 "%s/UPTA/Dataset.csv" % dir ]
+csvfile_list = [ "%s/test_data/DEDA/Dataset.csv" % dir,
+		 "%s/test_data/UPTA/Dataset.csv" % dir ]
 
 # List of names for sequnece alignment (optional)
 system_names = [ "DEDA", "UPTA" ]
-
-# List of input coordinate files - can be CG or AT
-#coord_files = [	"%s/Interaction_UDP1_UPTA/Coordinate_UDP1/Coordinate_UDP1_Occupancy.pdb" % dir,
-#		"%s/Interaction_UDP1_UPTA/Coordinate_UDP1/Coordinate_UDP1_Occupancy.pdb" % dir ]
-
-##########################
-### Set variables here ###
-##########################
-
-# Not currently needed
 
 #######################################
 ### Don't touch anything below here ###
@@ -87,8 +82,83 @@ def sequence_alignment():
 		for line in iter(process.stdout.readline, b''):
 			f.write(line.decode("utf-8"))
 
-def plot_heatmap_on_alignment():
-	pass
+# read alignment file into NumPy array
+table = str.maketrans('', '', string.ascii_lowercase)
+def reorder_alignment_for_plot(num):
+    new_sequence = []
+    with open('test.txt', "r") as ifile:
+        # this count keeps track of total lines
+        count = 0
+        # define one data series for each sequences used
+        for line_number,line in enumerate(ifile):
+            if 'sequence' in line:
+                # this bit simply removes non-sequence chars from the line
+                line = (line.translate(table).strip('\n '))
+                # save line with revised line number
+                line_array = [ line, count ]
+                new_sequence.append(line.translate(table).strip('\n '))
+                #print(new_sequence)'''
+                count=count+1
+    # count/num gives the lines per sequence
+    return count/num, new_sequence
+
+# reformat occupancy file
+def occupancy_to_new_file ():
+    occupancy = []
+    with open('Occupancy.txt','r') as ifile:
+            for line in ifile:
+                if '>' not in line:
+                    if '0' in line:
+                        occupancy.append(line.split( ))
+    f = open('Occupancy_reformatted.txt','w')
+    for system in np.arange(0,count):
+        residue_count = 0
+        for line in np.array(range(int(lines))):
+            sequence = new_sequence[system+(line*count)]
+            f = open('Occupancy_reformatted.txt','a')
+            for aa,char in enumerate(sequence):
+                if char != '-':
+                    f.write('%s ' % occupancy[system][residue_count])
+                    residue_count = residue_count+1
+                else:
+                    f.write(' -1 ')
+            f.write('\n')
+
+def get_occupancy_reordered(sequence, system, alignment_line, occupancy_count_in):
+    occupancy = []
+    with open('Occupancy_reformatted.txt','r') as ifile:
+        for line in ifile:
+                occupancy.append(line.split( ))
+    heatmap = []
+    sequence_array = []
+    occupancy_count = occupancy_count_in
+    residue_count = occupancy_count_in
+    for aa,char in enumerate(sequence):
+        if char != '-1':
+            heatmap.append(float(occupancy[alignment_line][aa]))
+            sequence_array.append(char)
+            occupancy_count = occupancy_count+1
+            residue_count = residue_count+1
+        else:
+            heatmap.append(-1)
+            sequence_array.append(char)
+    return heatmap, sequence_array, occupancy_count
+
+def plot_sequence(heatmap_array,alignment_line,line, sys1, sys2):
+    #takes an array of single letter AA codes and residue attribute values
+    axs[line].set_yticklabels(['',sys1,sys2])
+    axs[line].set_xticklabels('')
+    im = axs[line].imshow(heatmap_array, cmap='Reds', vmin=0, vmax=100 )
+
+def add_labels(text_array_1,text_array_2,alignment_line,line):
+    for x,y in enumerate(np.arange(len(text_array_1))):
+        axs[line].text(x-0.25,0.25, text_array_1[x], fontsize=10)
+    for x,y in enumerate(np.arange(len(text_array_2))):
+        axs[line].text(x-0.25,(1.25), text_array_2[x], fontsize=10)
+
+def add_ticks(offset,line):
+    for x,y in enumerate(np.arange(0,60,step=10)):
+        axs[line].text(y-0.4,2.5, y+offset, fontsize=10)
 
 ######################
 ### Run everything ###
@@ -134,11 +204,40 @@ sequence_alignment()
 
 print('Done. Alignment file written to %s/HeatmapAlignment' % dir)
 
+# reformat data for plotting
+
+print('Reformating files for plotting')
+
+lines, new_sequence = reorder_alignment_for_plot(count)
+occupancy_to_new_file()
+
+print('Done. Reformatted files written to %s/HeatmapAlignment' % dir)
+
 # combine all into a lovely plot
 
 print('Making plot')
 
-# this is currently in a separate script
-plot_heatmap_on_alignment()
+fig = matplotlib.pyplot.gcf()
+fig.set_size_inches(lines*(count+1), 6)
+gs = fig.add_gridspec(int(lines),1, hspace=0)
+axs = gs.subplots(sharex=True)
 
-print('Done')
+a = np.array(range(int(lines)))
+occupancy_count = 0 
+a_line = 0
+for system in np.arange(0,count):
+    for line in np.arange(2):
+        heatmap_array = []
+        text_array = []
+        alignment_line = line*count+system
+        print(new_sequence[alignment_line], system, occupancy_count)
+        heatmap_values, sequence_array, occupancy_count = get_occupancy_reordered(
+            new_sequence[alignment_line], system, a_line, occupancy_count)
+        heatmap_array.append(heatmap_values)
+        text_array.append(sequence_array)
+        a_line = a_line + 1
+        print(heatmap_array)
+        plot_sequence(heatmap_array, alignment_line, line, sys1, sys2)
+    add_labels(text_array[0],text_array[1],alignment_line, line)
+
+print('Plot finished, exiting program')
